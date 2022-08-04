93697e9035b1cc6e01d8cc26298991e2ce6e9be6
sdineen <dineen701@gmail.com>
2022-08-04 14:49:52 +0100

    sql

--- src/main/java/com/qa/jdbc/ProductRepository.java
+++ src/main/java/com/qa/jdbc/ProductRepository.java
@@ -16,9 +16,11 @@ public class ProductRepository {
 		//autocloseable object
 		try (Connection c = DriverManager.getConnection(url, user, password)) {
 			Statement statement = c.createStatement();
-			String sql = "insert";
+			String sql = "insert into product (id, name, costprice, retailprice) values ("
+					+product.getId()+",'"+product.getName()+"',"+product.getCostPrice()
+					+","+product.getRetailPrice()+");";
 			int rowCount = statement.executeUpdate(sql);		
-			//return true if rowCount is 1
+			return rowCount==1;
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
